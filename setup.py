b11df14b3f861709dc88b8af4855bf7f5fd9441a
Paul Rysiavets <paul@covadem.com>
2025-04-09 15:28:11 +0200

    ci(PyPI): init package publishing

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('README.md', encoding='utf-8') as file:
     readme = file.read()
 
 setuptools.setup(
-    name='fastapi-auth0',
-    version='0.5.0',
+    name='fastapi-auth0-pyjwt',
+    version='0.6.0',
     description='Easy auth0.com integration for FastAPI',
     long_description=readme,
     long_description_content_type='text/markdown',
-    url='https://github.com/dorinclisu/fastapi-auth0',
+    url='https://github.com/spawn-guy/fastapi-auth0',
     author='Dorin Clisu',
     license='MIT',
     packages=setuptools.find_packages('src'),
