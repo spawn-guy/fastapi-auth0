8f43eb28263c1ea1a96c76ee92a8e4d134686f2c
Paul Rysiavets <paul@covadem.com>
2025-04-09 15:43:10 +0200

    ci(PyPI): update maintainer

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import setuptools
 
 with open('README.md', encoding='utf-8') as file:
@@ -6,12 +5,13 @@ with open('README.md', encoding='utf-8') as file:
 
 setuptools.setup(
     name='fastapi-auth0-pyjwt',
-    version='0.6.1',
-    description='Easy auth0.com integration for FastAPI',
+    version='0.6.2',
+    description='Easy auth0.com integration for FastAPI and PyJWT',
     long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/spawn-guy/fastapi-auth0',
     author='Dorin Clisu',
+    maintainer='Paul Rysiavets',
     license='MIT',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
